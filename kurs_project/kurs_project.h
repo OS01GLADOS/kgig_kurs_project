90f700938457e3e5512ae6b70ef7034517ddc43b
OS01GLADOS <54987917+OS01GLADOS@users.noreply.github.com>
2020-10-21 11:16:31 +0300

    project created

---
+++ kurs_project/kurs_project.h
@@ -0,0 +1,35 @@
+﻿
+// kurs_project.h: основной файл заголовка для приложения kurs_project
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // основные символы
+
+
+// CkursprojectApp:
+// Сведения о реализации этого класса: kurs_project.cpp
+//
+
+class CkursprojectApp : public CWinApp
+{
+public:
+	CkursprojectApp() noexcept;
+
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Реализация
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CkursprojectApp theApp;
